7b3d714633d1d2a9a4222ff216ec9b89ec5de9d4
Daria <d.vorobiienko@student.khai.edu>
2021-10-03 19:08:29 +0300

    Test.java added to feature_branch with change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,11 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java education");
 
+=======
+        System.out.println("education");
+        //new string
+>>>>>>> b073633 (Test.java added to feature_branch with change on string #2)
     }
 }
