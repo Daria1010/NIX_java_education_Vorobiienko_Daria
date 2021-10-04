5d89da44036e7b2a7cdb9684fd1f335df341c62a
Daria <d.vorobiienko@student.khai.edu>
2021-10-04 13:59:40 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Java education");
-
-=======
-        System.out.println("education");
         //new string
->>>>>>> b073633 (Test.java added to feature_branch with change on string #2)
+        System.out.println("DICT");
     }
 }
